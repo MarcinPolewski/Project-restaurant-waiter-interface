74af97d925837d3544cb1c11d2e82b8ff37a7d33
Michał Gryglicki <michal.gryglicki2.stud@pw.edu.pl>
2024-06-09 21:33:59 +0200

    Moved address class methods definitions to .cpp file

--- include/address.h
+++ include/address.h
@@ -11,15 +11,7 @@ struct Address
     const std::string additionalInfo;
 
     Address(const std::string &cty, const std::string post_code,
-            const std::string strt, const std::string &num, const std::string &AI = "")
-        : city(cty), postalCode(post_code), street(strt), number(num), additionalInfo(AI) {}
+            const std::string strt, const std::string &num, const std::string &AI = "");
 
-    std::string str() const
-    {
-        std::string output = this->street + " " + this->number + "\n" + this->postalCode + " " + this->city;
-        if (additionalInfo.empty())
-            return output;
-        output += "\nAI: " + this->additionalInfo;
-        return output;
-    }
+    std::string getStr() const;
 };
